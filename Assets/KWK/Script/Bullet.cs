daa3853f31ec4d115345edb1fad3deaf296912a1
cirkle0411 <nakiie1568@gmail.com>
2025-10-25 18:20:17 +0900

    day3

--- Assets/KWK/Script/Bullet.cs
+++ Assets/KWK/Script/Bullet.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed = 20f;
-    public int damage = 4;
-
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private int damage = 4;
+    
+    private float lifetime = 3f;
     private Transform target;
     private Rigidbody2D rb;
 
@@ -14,6 +15,7 @@ public class Bullet : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, lifetime);
     }
 
     // Update is called once per frame
@@ -34,21 +36,16 @@ public class Bullet : MonoBehaviour
             Destroy(gameObject);
             return;
         }
-        
-        Vector2 direction = (target.position - transform.position).normalized;
+
+        Vector2 direction = (Vector2)(target.position - transform.position).normalized;
+
         rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Monster"))
+        if (other.CompareTag("Monster"))
         {
-            Monster monster = other.GetComponent<Monster>();
-            if (monster != null)
-            {
-                monster.TakeDamage(damage);
-            }
-            
             Destroy(gameObject);
         }
     }
---
+++ Assets/KWK/Script/ChocochipTurret.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChocochipTurret : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private float interval = 1f;
+
+    private List<Collider2D> targets = new List<Collider2D>();
+    private Coroutine shootCoroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(!other.CompareTag("Monster"))
+        {
+            return;
+        }
+
+        if(!targets.Contains(other))
+        {
+            targets.Add(other);
+        }
+
+        if(shootCoroutine == null)
+        {
+            shootCoroutine = StartCoroutine(shoot());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(!other.CompareTag("Monster"))
+        {
+            return;
+        }
+
+        targets.Remove(other);
+
+        if(targets.Count == 0 && shootCoroutine != null)
+        {
+            StopCoroutine(shootCoroutine);
+            shootCoroutine = null;
+        }
+    }
+
+    private IEnumerator shoot()
+    {
+        while (targets.Count > 0)
+        {
+            GameObject instance = Instantiate(prefab, shootPoint.position, shootPoint.rotation);
+            Bullet bullet = instance.GetComponent<Bullet>();
+            
+            if (bullet != null)
+            {
+                bullet.SetTarget(targets[0].transform);
+            }
+
+            yield return new WaitForSeconds(interval);
+        }
+
+        shootCoroutine = null;
+    }
+}
