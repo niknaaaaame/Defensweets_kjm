8553f665de2166a671e9bb7d83498735a244c835
niknaaaaame <ellic3031@gmail.com>
2025-11-08 17:48:05 +0900

    25.11.28

--- Assets/KJM/scripts/TileEditor.cs
+++ Assets/KJM/scripts/TileEditor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class Tile : MonoBehaviour
+public class TileEditor : MonoBehaviour
 {
     public MapSO mapData;
 
@@ -18,6 +18,9 @@ public class Tile : MonoBehaviour
     private int arrayX;
     private int arrayY;
 
+    private const int PATH = 0; 
+    private const int BLOCK = 1; 
+
     void Start()
     {
         tileData = new int[mapData.mapWidth, mapData.mapHeight];
@@ -27,7 +30,7 @@ public class Tile : MonoBehaviour
         {
             for (int y = 0; y < mapData.mapHeight; y++)
             {
-                tileData[x, y] = 1;
+                tileData[x, y] = BLOCK;
                 isSpecialTile[x, y] = false;
             }
         }
@@ -62,7 +65,7 @@ public class Tile : MonoBehaviour
             {
                 for (int y = 0; y < mapData.mapHeight; y++)
                 {
-                    tileData[x, y] = 1;
+                    tileData[x, y] = BLOCK;
                     Vector3Int tilePos = new Vector3Int(x + tilemap.origin.x, y + tilemap.origin.y, 0);
                     tilemap.SetTile(tilePos, isSpecialTile[x, y] ? specialTile : groundTile);
                 }
@@ -90,28 +93,29 @@ public class Tile : MonoBehaviour
 
         if (place)
         {
-            if (CanRestorationTile(arrayX, arrayY))
-            {
-                tilemap.SetTile(cellPos, isSpecialTile[arrayX, arrayY] ? specialTile : groundTile);
-                tileData[arrayX, arrayY] = 1;
-            }
+            tilemap.SetTile(cellPos, isSpecialTile[arrayX, arrayY] ? specialTile : groundTile);
+            tileData[arrayX, arrayY] = BLOCK;
+            //ResourceSystem.Instance.AddCrystal(1);
+            
         }
         else
         {
             if (CanExploitationTile(arrayX, arrayY))
             {
                 tilemap.SetTile(cellPos, null);
-                tileData[arrayX, arrayY] = 0;
+                tileData[arrayX, arrayY] = PATH;
+                ResourceSystem.Instance.TryUseCrystal(2);
+                Debug.Log($"Crystal: {ResourceSystem.Instance.Crystal}");
             }
         }
     }
 
     bool CanExploitationTile(int x, int y)
     {
-        if (x == mapData.startPos.x && y == mapData.startPos.y && tileData[x, y] == 1)
+        if (x == mapData.startPos.x && y == mapData.startPos.y && tileData[x, y] == BLOCK)
             return true;
 
-        if (tileData[x, y] == 0)
+        if (tileData[x, y] == PATH)
             return false;
 
         Vector2Int[] directions = { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
@@ -121,52 +125,57 @@ public class Tile : MonoBehaviour
             int ny = y + dir.y;
             if (nx >= 0 && nx < mapData.mapWidth && ny >= 0 && ny < mapData.mapHeight)
             {
-                if (tileData[nx, ny] == 0)
+                if (tileData[nx, ny] == PATH)
                     return true;
             }
         }
         return false;
     }
 
-    bool CanRestorationTile(int x, int y)
-    {
-        if (x == mapData.startPos.x && y == mapData.startPos.y)
-        {
-            Vector2Int[] mustBeFilled = { Vector2Int.up, Vector2Int.down, Vector2Int.right };
-            foreach (var dir in mustBeFilled)
-            {
-                int nx = x + dir.x;
-                int ny = y + dir.y;
-                if (nx < 0 || nx >= mapData.mapWidth || ny < 0 || ny >= mapData.mapHeight || tileData[nx, ny] != 1)
-                    return false;
-            }
-            return true;
-        }
+    //bool CanRestorationTile(int x, int y)
+    //{
+    //    if (x == mapData.startPos.x && y == mapData.startPos.y)
+    //    {
+    //        Vector2Int[] mustBeFilled = { Vector2Int.up, Vector2Int.down, Vector2Int.right };
+    //        foreach (var dir in mustBeFilled)
+    //        {
+    //            int nx = x + dir.x;
+    //            int ny = y + dir.y;
+    //            if (nx < 0 || nx >= mapData.mapWidth || ny < 0 || ny >= mapData.mapHeight || tileData[nx, ny] != 1)
+    //                return false;
+    //        }
+    //        return true;
+    //    }
 
-        if (tileData[x, y] == 1) return false;
+    //    if (tileData[x, y] == 1) return false;
 
-        Vector2Int[] directions = { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
-        int emptyCount = 0;
+    //    Vector2Int[] directions = { Vector2Int.up, Vector2Int.down, Vector2Int.left, Vector2Int.right };
+    //    int emptyCount = 0;
 
-        foreach (var dir in directions)
-        {
-            int nx = x + dir.x;
-            int ny = y + dir.y;
+    //    foreach (var dir in directions)
+    //    {
+    //        int nx = x + dir.x;
+    //        int ny = y + dir.y;
 
-            if (nx < 0 || nx >= mapData.mapWidth || ny < 0 || ny >= mapData.mapHeight)
-            {
-                emptyCount++;
-                continue;
-            }
+    //        if (nx < 0 || nx >= mapData.mapWidth || ny < 0 || ny >= mapData.mapHeight)
+    //        {
+    //            emptyCount++;
+    //            continue;
+    //        }
 
-            if (tileData[nx, ny] == 1) emptyCount++;
-        }
+    //        if (tileData[nx, ny] == 1) emptyCount++;
+    //    }
 
-        return emptyCount >= 3;
-    }
+    //    return emptyCount >= 3;
+    //}
 
     bool CheckComplete()
     {
-        return tileData[mapData.goalPos.x, mapData.goalPos.y] == 0;
+        Vector3 startWorldPos = tilemap.GetCellCenterWorld(new Vector3Int(mapData.startPos.x, mapData.startPos.y, 0));
+        Vector3 goalWorldPos = tilemap.GetCellCenterWorld(new Vector3Int(mapData.goalPos.x, mapData.goalPos.y, 0));
+
+        List<Vector3> path = BFS.FindPath(startWorldPos, goalWorldPos);
+
+        return path != null && path.Count > 0;
     }
 }
