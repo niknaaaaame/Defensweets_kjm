9a27ebba9dd38f61b5fd1908a9d4ab866531cfbc
niknaaaaame <ellic3031@gmail.com>
2025-12-24 17:45:20 +0900

    CollectionScene

---
+++ Assets/KJM/scripts/Collection/CollectionData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Defensweet/Collection")]
+public class CollectionData : ScriptableObject
+{
+    [Header("Image")]
+    public Sprite image;
+
+    [Header("Name")]
+    public string displayName;
+
+    [Header("Description")]
+    [TextArea(5, 15)]
+    public string description;
+}
