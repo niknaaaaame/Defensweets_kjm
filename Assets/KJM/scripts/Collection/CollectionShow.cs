827991bfe9c23a62e4beb9da11d1b3d9b98ea38e
niknaaaaame <ellic3031@gmail.com>
2026-01-09 17:37:50 +0900

    Revert "collectionLock"
    This reverts commit 0aedf2df79ac3ae8fa27794c69856e0c9a353267.

--- Assets/KJM/scripts/Collection/CollectionShow.cs
+++ Assets/KJM/scripts/Collection/CollectionShow.cs
@@ -15,14 +15,10 @@ public class CollectionShow : MonoBehaviour
     public Text descriptionText;
     public Image entryImage;
     public Image rangeImage;
-    public GameObject lockImage;
-    public GameObject[] lockedImages;
 
     [Header("UI Roots")]
     public GameObject iconRoot;
     public GameObject entryDetailRoot;
-
-    public int unlockCount = 3;
 
     void Start()
     {
@@ -33,12 +29,6 @@ public class CollectionShow : MonoBehaviour
     {
         iconRoot.SetActive(true);
         entryDetailRoot.SetActive(false);
-
-        for (int i = 0; i < lockedImages.Length; i++)
-        {
-            bool isLocked = i >= unlockCount;
-            lockedImages[i].SetActive(isLocked);
-        }
     }
 
     void ShowEntryView()
@@ -51,40 +41,22 @@ public class CollectionShow : MonoBehaviour
     {
         if (entries.Length == 0) return;
 
-        ShowEntryView();
+        ShowEntryView(); 
 
         currentIndex = Mathf.Clamp(index, 0, entries.Length - 1);
         var data = entries[currentIndex];
 
-        bool unlocked = currentIndex < unlockCount;
-
-        if (unlocked)
-        {
-            nameText.text = data.displayName;
-            descriptionText.text = data.description;
-            entryImage.sprite = data.image;
-            rangeImage.sprite = data.rangeImage;
-
-            lockImage.SetActive(false);
-        }
-        else
-        {
-            nameText.text = "미해금";
-            descriptionText.text = "미해금";
-            entryImage.sprite = null;    
-            rangeImage.sprite = null;
-
-            lockImage.SetActive(true);
-        }
+        nameText.text = data.displayName;
+        descriptionText.text = data.description;
+        entryImage.sprite = data.image;
+        rangeImage.sprite = data.rangeImage;
     }
 
     public void NextPage()
     {
-        if (unlockCount <= 0) return;
-
         int nextIndex = currentIndex + 1;
 
-        if (nextIndex >= unlockCount)
+        if (nextIndex >= entries.Length)
             nextIndex = 0;
 
         ShowEntry(nextIndex);
@@ -92,12 +64,10 @@ public class CollectionShow : MonoBehaviour
 
     public void PrevPage()
     {
-        if (unlockCount <= 0) return;
-
         int prevIndex = currentIndex - 1;
 
         if (prevIndex < 0)
-            prevIndex = unlockCount - 1;
+            prevIndex = entries.Length - 1;
 
         ShowEntry(prevIndex);
     }
